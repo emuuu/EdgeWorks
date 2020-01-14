3b88384cd976966ba9cfeafa9fd3cc4582450ec6
Timm <tb@fm-gruppe.com>
2020-01-14 23:26:08 +0100

    started analysation tests

--- src/EdgeWorks.Tools/Services/AnalyseService.cs
+++ src/EdgeWorks.Tools/Services/AnalyseService.cs
@@ -1,9 +1,12 @@
 ﻿using EdgeWorks.Models.Auctions;
+using EdgeWorks.Models.Items;
 using EdgeWorks.Shared.Configurations.BlizzardAPIs.WorldOfWarcraft.GameDataAPIs;
 using EdgeWorks.Shared.Helpers;
 using EdgeWorks.Shared.Services.Authentication;
 using EdgeWorks.Shared.Services.Files;
+using EdgeWorks.Statistics.Statistics;
 using EdgeWorks.Tools.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -26,7 +29,35 @@ namespace EdgeWorks.Tools.Services
             _itemApi = itemApi;
         }
 
-       public async Task GetFiles()
+        public async Task GetItemStatistic(int itemID)
+        {
+            Console.WriteLine("Select sample file:");
+            var auctionData = await GetAuctionFile();
+            var item = await GetItem(itemID);
+            var auctions = auctionData.Auctions.Where(x => x.Item == itemID);
+            //var statistic = new StatisticItem(auctions.SelectMany(x => Enumerable.Repeat(x, x.Quantity).Select(y => y.Buyout / 10000.0)).ToList());
+            var statistic = new StatisticItem(auctions.Select(y => y.Buyout / 10000.0).ToList());
+
+            Console.WriteLine("");
+            Console.WriteLine("{0}", item.Name, "00CC66".ToColor());
+            Console.WriteLineFormatted("Sum: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Sum, 2)));
+            Console.WriteLineFormatted("Count: {0}", Color.White, TypeDictionary.FormatInput(statistic.Count));
+            Console.WriteLineFormatted("ArithmeticMean: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.ArithmeticMean, 2)));
+            Console.WriteLineFormatted("Median: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Median, 2)));
+            Console.WriteLineFormatted("Minimum: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Minimum, 2)));
+            Console.WriteLineFormatted("Maxmimum: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Maxmimum, 2)));
+            Console.WriteLineFormatted("Range: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Range, 2)));
+            Console.WriteLineFormatted("Variance: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.Variance, 2)));
+            Console.WriteLineFormatted("StandardDeviation: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.StandardDeviation, 2)));
+            Console.WriteLineFormatted("SampleVariance: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.SampleVariance, 2)));
+            Console.WriteLineFormatted("SampleStandardDeviation: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.SampleStandardDeviation, 2)));
+            Console.WriteLineFormatted("LowerQuartile: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.LowerQuartile, 2)));
+            Console.WriteLineFormatted("HigherQuartile: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.HigherQuartile, 2)));
+            Console.WriteLineFormatted("QuartileDistance: {0}", Color.White, TypeDictionary.FormatInput(Math.Round(statistic.QuartileDistance, 2)));
+
+        }
+
+        public async Task<AuctionData> GetAuctionFile()
         {
             var i = 1;
             var files = (await _fileService.GetStorage()).Select(x => new KeyValuePair<int, FileInfo>(i++, new FileInfo(x))).ToList();
@@ -47,25 +78,32 @@ namespace EdgeWorks.Tools.Services
                         Console.WriteLine("Exiting filestorage");
                         break;
                     }
-
-                    var aucions = await _fileService.LoadFromStorage<AuctionData>(files.First(x => x.Key == key).Value.Name);
-                    Console.WriteFormatted("Succesfully loaded auction data for {0}", Color.White, TypeDictionary.FormatInput(aucions.Realms.First().Name));
-                    Console.WriteLine();
+                    Console.WriteLine("");
+                    return await _fileService.LoadFromStorage<AuctionData>(files.First(x => x.Key == key).Value.Name);
                 }
                 else
                 {
                     Console.WriteLine("Exiting filestorage");
+                    Console.WriteLine("");
                     break;
                 }
             }
+            return null;
         }
 
-        public async Task GetItem(int itemID)
+        public async Task<Item> GetItem(int itemID)
         {
-            var accessToken = await _tokenService.GetAccessToken();
-            var response = await HttpRequestFactory.Get(accessToken, _itemApi.Item(itemID));
-
-            Console.WriteLine(response.ContentAsJson());
+            try
+            {
+                var accessToken = await _tokenService.GetAccessToken();
+                var response = await HttpRequestFactory.Get(accessToken, _itemApi.Item(itemID));
+                return response.ContentAsType<Item>();
+            }
+            catch (Exception ex)
+            {
+                ex.PrintException();
+                return null;
+            }
         }
     }
 }
